a4fda60add7e6777900df68a5964c4e27de1daf6
xonnex <xonnex@pm.me>
2024-12-03 15:37:20 +0100

    day2 solved

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,12 +1,64 @@
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use itertools::Itertools;
 
-///////////////////////////////////////////////////////////////////////////////
+pub fn solve(input: &str) -> SolutionPair {
+    let (mut p1, mut p2) = (0, 0);
+    for line in input.lines() {
+        let values = line
+            .split_whitespace()
+            .map(str::parse::<i32>)
+            .filter_map(Result::ok)
+            .collect::<Vec<_>>();
 
-pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+        if check_report_safe(&values) {
+            p1 += 1;
+        }
 
-    (Solution::from(sol1), Solution::from(sol2))
+        if brute_force(&values) {
+            p2 += 1;
+        }
+    }
+
+    (Solution::from(p1), Solution::from(p2))
+}
+
+fn brute_force(report: &[i32]) -> bool {
+    for i in 0..report.len() {
+        let mut test = report.to_vec();
+        test.remove(i);
+        if check_report_safe(&test) {
+            return true;
+        }
+    }
+    false
+}
+
+fn check_report_safe(report: &[i32]) -> bool {
+    let mut ok = false;
+
+    // check if ascending
+    ok |= report.iter().tuple_windows().all(|(a, b)| a < b);
+
+    // check if descending
+    ok |= report.iter().tuple_windows().all(|(a, b)| a > b);
+
+    // check if difference is 3 or less
+    ok && report.iter().tuple_windows().all(|(a, b)| {
+        let diff = a - b;
+        diff.abs() <= 3
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::etc::solution::Solution;
+
+
+    #[test]
+    fn test_sample_input() {
+        let input = include_str!("../../input/day02/test.txt");
+        let (part1, part2) = super::solve(input);
+        assert_eq!(part1, Solution::I32(2));
+        assert_eq!(part2, Solution::I32(4));
+    }
 }
