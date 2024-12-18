ecf71fe09f9e66d2457a882da384a611a5892f0f
xonnex <xonnex@pm.me>
2024-12-18 10:00:53 +0100

    day18 solved

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,13 +1,109 @@
+use std::{collections::VecDeque, vec};
+
+use hashbrown::HashSet;
+use itertools::Itertools;
+
 use crate::{Solution, SolutionPair};
 
-///////////////////////////////////////////////////////////////////////////////
+type Vector2 = (usize, usize);
 
-pub fn solve(_input: &str) -> SolutionPair {
+#[derive(Debug, Default, Clone)]
+struct Grid {
+    grid: Vec<Vec<u8>>,
+}
+
+impl Grid {
+    fn of_size(size: usize) -> Self {
+        let grid = vec![vec![b'.'; size]; size];
+
+        Self { grid }
+    }
+
+    fn get(&self, (x, y): Vector2) -> Option<u8> {
+        self.grid.get(y).and_then(|row| row.get(x).copied())
+    }
+
+    fn set(&mut self, x: usize, y: usize, value: u8) {
+        self.grid[y][x] = value;
+    }
+}
+
+pub fn solve(input: &str) -> SolutionPair {
     // Your solution here...
-    let p1 = 0;
-    let p2 = 0;
+    let mut grid = Grid::of_size(71);
 
-    (Solution::from(p1), Solution::from(p2))
+    let mut coords = input
+        .split(|c: char| !c.is_ascii_digit() && c != '-')
+        .filter(|w| !w.is_empty())
+        .map(|w| w.parse::<usize>().unwrap())
+        .tuples();
+
+    for _ in 0..1024 {
+        let (x, y) = coords.next().unwrap();
+        grid.set(x, y, b'#');
+    }
+
+    let p1 = bfs(&grid, (70, 70), (0, 0)).unwrap();
+
+    let mut grid = grid.clone();
+    let mut p2 = String::from("(0,0)");
+    for (x, y) in coords {
+        grid.set(x, y, b'#');
+        if let None = dfs(&grid, (70, 70), (0, 0)) {
+            p2 = format!("{},{}", x, y);
+            break;
+        }
+    }
+
+    (Solution::from(p1), Solution::Str(p2.into()))
+}
+
+fn bfs(grid: &Grid, start: Vector2, end: Vector2) -> Option<usize> {
+    let mut q = VecDeque::from([(0, start)]);
+    let mut visited = HashSet::new();
+
+    while let Some((steps, p)) = q.pop_front() {
+        if p == end {
+            return Some(steps);
+        }
+
+        if !visited.insert(p) {
+            continue;
+        }
+
+        let (x, y) = p;
+        for next in [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)] {
+            if let Some(b'.') = grid.get(next) {
+                q.push_back((steps + 1, next));
+            }
+        }
+    }
+
+    None
+}
+
+fn dfs(grid: &Grid, start: Vector2, end: Vector2) -> Option<usize> {
+    let mut q = vec![(0, start)];
+    let mut visited = HashSet::new();
+
+    while let Some((steps, p)) = q.pop() {
+        if p == end {
+            return Some(steps);
+        }
+
+        if !visited.insert(p) {
+            continue;
+        }
+
+        let (x, y) = p;
+        for next in [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)] {
+            if let Some(b'.') = grid.get(next) {
+                q.push((steps + 1, next));
+            }
+        }
+    }
+
+    None
 }
 
 #[cfg(test)]
@@ -18,8 +114,36 @@ mod tests {
     fn test_sample_input() {
         let input = include_str!("../../input/day18/test.txt");
 
-        let (p1, p2) = super::solve(input);
-        assert_eq!(p1, Solution::Usize(0));
-        assert_eq!(p2, Solution::Usize(0));
+        let start = (0, 0);
+        let end = (6, 6);
+        let instructions = input
+            .lines()
+            .map(|l| {
+                l.split_once(',')
+                    .map(|(x, y)| (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap()))
+                    .unwrap()
+            })
+            .collect::<Vec<_>>();
+
+        let mut grid = Grid::of_size(7);
+        for instruction in instructions.iter().take(12) {
+            grid.set(instruction.0, instruction.1, b'#');
+        }
+        let steps = super::bfs(&grid, start, end);
+
+        assert_eq!(steps, Some(22));
+
+        let mut grid = grid.clone();
+
+        let mut p2 = String::from("(0,0)");
+        for &(x, y) in instructions.iter().skip(12) {
+            grid.set(x, y, b'#');
+            if let None = bfs(&grid, start, end) {
+                p2 = format!("{},{}", x, y);
+                break;
+            }
+        }
+
+        assert_eq!(p2, "6,1");
     }
 }
